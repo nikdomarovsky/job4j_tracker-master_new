5986b6ee1caad2213b9f6e3734991a20b3a75cf7
Администратор <nikdomarovsky@gmail.com>
2024-10-25 21:25:01 +0300

    1.2. Вызов метода объекта

---
+++ job4j_tracker-master/src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+public class Student {
+
+    public void music() {
+        System.out.println("Tra tra tra");
+        System.out.println("I believe I can fly");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+    }
+}
