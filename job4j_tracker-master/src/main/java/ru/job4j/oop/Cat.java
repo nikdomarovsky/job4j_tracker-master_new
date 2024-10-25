9cebf485249e242116641971a194598fa3fda4b7
Администратор <nikdomarovsky@gmail.com>
2024-10-25 21:53:07 +0300

    1.5. Поля объекта

--- job4j_tracker-master/src/main/java/ru/job4j/oop/Cat.java
+++ job4j_tracker-master/src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,35 @@
 package ru.job4j.oop;
-
 public class Cat {
+    private String food;
+    private String name;
+    public void show() {
+        System.out.println(this.food);
+        System.out.println(this.name);
+    }
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+    public String sound() {
+        String voice = "may-may";
+        return voice;
+    }
+    public void eat(String meat) {
+        this.food = meat;
+    }
     public static void main(String[] args) {
-        Cat petty = new Cat();
+        Cat peppy = new Cat();
         Cat sparky = new Cat();
+        String say = peppy.sound();
+        System.out.println("Peppy says " + say);
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("gav");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("black");
+        black.show();
     }
 }
